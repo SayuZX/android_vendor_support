f8ed7c730ea462baa98bcd672528e41e4c644b15
ProjectKernel <155278452+KernelBuilding@users.noreply.github.com>
2024-03-12 11:35:25 +0700

    Bringup

--- src/com/scandium/support/preferences/TwoTargetPreference.java
+++ src/com/scandium/support/preferences/TwoTargetPreference.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.legion.support.preferences;
+package com.scandium.support.preferences;
 
 import android.content.Context;
 import androidx.preference.Preference;
@@ -22,7 +22,7 @@ import androidx.preference.PreferenceViewHolder;
 import android.util.AttributeSet;
 import android.view.View;
 
-import com.legion.support.R;
+import com.scandium.support.R;
 
 public class TwoTargetPreference extends Preference {
 
