facea82c554f830c4410fdfd36ecef0d2130720f
ProjectKernel <155278452+KernelBuilding@users.noreply.github.com>
2024-03-12 11:24:42 +0700

    Bringup

--- src/com/scandium/support/preferences/GlobalSettingSeekBarPreference.java
+++ src/com/scandium/support/preferences/GlobalSettingSeekBarPreference.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.legion.support.preferences;
+package com.scandium.support.preferences;
 
 import android.content.Context;
 import android.util.AttributeSet;
