6b7c36d6b98dc6d09edba84155609fe6994f5474
ProjectKernel <155278452+KernelBuilding@users.noreply.github.com>
2024-03-12 11:33:18 +0700

    Bringup

--- src/com/scandium/support/preferences/SystemSettingIntListPreference.java
+++ src/com/scandium/support/preferences/SystemSettingIntListPreference.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.legion.support.preferences;
+package com.scandium.support.preferences;
 
 import android.content.Context;
 import android.util.AttributeSet;
